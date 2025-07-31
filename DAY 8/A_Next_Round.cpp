7ff2e7520116fe81bb8988a10afb5b665f4ae746
Prem Kolte <premkolte26@gmail.com>
2025-08-01 03:57:14 +0530

    DAY 8

---
+++ DAY 8/A_Next_Round.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    
+    int kth = a[k-1];
+    
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] > 0 && a[i] >= kth) {
+            ans++;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
