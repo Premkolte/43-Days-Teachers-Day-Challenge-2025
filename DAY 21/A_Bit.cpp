de78e04ee7683217a22dbd3b846fea098aafb176
Prem Kolte <premkolte26@gmail.com>
2025-09-02 12:22:40 +0530

    DAY 21

---
+++ DAY 21/A_Bit.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;cin>>t;
+    int x=0;
+    while(t--){
+        string s;cin>>s;
+
+        if(s[1]=='+'){
+            x++;
+        }
+        else if(s[1]=='-'){
+            x--; 
+        }
+    }
+    cout<<x<<endl;
+    return 0;
+}
