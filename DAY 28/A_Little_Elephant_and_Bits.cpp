121fb7642aa2be9df53b7c45049a78561217f784
Prem Kolte <premkolte26@gmail.com>
2025-09-05 14:12:04 +0530

    DAY 28

---
+++ DAY 28/A_Little_Elephant_and_Bits.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int n = s.length();
+
+    for(int i=0;i<n;i++){
+        if(s[i]=='0'){
+            s.erase(i, 1);
+            cout << s << endl;
+            return 0;
+        }
+    }
+    s.erase(0, 1);
+    cout << s << endl;
+}
---
+++ DAY 28/A_Magic_Numbers.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int n = s.length();
+
+    for(int i = 0; i < n; ) {
+        if(s.substr(i, 3) == "144") {
+            i += 3;
+        }
+        else if(s.substr(i, 2) == "14") {
+            i += 2;
+        }
+        else if(s[i] == '1') {
+            i += 1;
+        }
+        else {
+            cout << "NO" << endl;
+            return 0;
+        }
+    }
+    cout << "YES" << endl;
+}
